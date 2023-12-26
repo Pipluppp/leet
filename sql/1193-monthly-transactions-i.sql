7b8837e63c9e2c2bf4cd3ed24eb778a441f56c56
Pipluppp <duncanb013@gmail.com>
2023-12-26 19:07:09 +0800

    Use FILTER instead of case-when

--- sql/1193-monthly-transactions-i.sql
+++ sql/1193-monthly-transactions-i.sql
@@ -1,6 +1,9 @@
--- https://leetcode.com/problems/1193-monthly-transactions-i
-SELECT month, country, COUNT(*) AS trans_count, SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+-- https://leetcode.com/problems/monthly-transactions-i
+SELECT month, country, COUNT(*) AS trans_count,
+    -- Used FILTER instead of cases
+    COUNT(*) FILTER (WHERE state = 'approved') AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+    -- Needs COALESCE to turn NULL (when no row passes FILTER) into 0
+    COALESCE(SUM(amount) FILTER (WHERE state = 'approved'), 0) AS approved_total_amount
 FROM (SELECT *, to_char(trans_date, 'YYYY-MM') AS month FROM Transactions)
 GROUP BY country, month
